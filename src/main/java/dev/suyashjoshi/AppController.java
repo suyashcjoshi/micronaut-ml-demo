cb4cc6b5a061cec37c6ba500897d2fed730bd067
Suyash Joshi <suyash@suyashjoshi.com>
2020-12-04 11:48:30 -0800

    Hello World compelte

---
+++ src/main/java/dev/suyashjoshi/AppController.java
@@ -0,0 +1,14 @@
+package dev.suyashjoshi;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello") 
+public class AppController {
+
+    @Get(produces = MediaType.TEXT_PLAIN) 
+    public String index() {
+        return "Hello World"; 
+    }
+}
