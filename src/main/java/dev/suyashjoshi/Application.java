b40bd8ddc626c2a08513206b0a8c224e84dc34e0
Suyash Joshi <suyashjoshi>
2020-12-04 18:52:29 +0000

    Initial commit

---
+++ src/main/java/dev/suyashjoshi/Application.java
@@ -0,0 +1,10 @@
+package dev.suyashjoshi;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/dev/suyashjoshi/MlDemoTest.java
@@ -0,0 +1,21 @@
+package dev.suyashjoshi;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MlDemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
